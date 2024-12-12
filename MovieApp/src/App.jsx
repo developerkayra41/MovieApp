c246d51fadb04ef5359d9326234f26af866d48fa
Kayra Özgür <kayra.sagopa23@hotmail.com>
2024-12-12 13:54:22 +0300

    .

--- MovieApp/src/App.jsx
+++ MovieApp/src/App.jsx
@@ -1,11 +1,12 @@
 
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
-    <>
-      selam
-    </>
+    <div className='app'>
+      <Navbar />
+    </div>
   )
 }
 
